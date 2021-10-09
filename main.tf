f084d0fc7c2309ed4045182ca2bbea7ae292e9cf
anubys17 <54693678+anubys17@users.noreply.github.com>
2021-10-09 14:31:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "random_pet" "name" {}
 resource "aws_instance" "web" {
   ami           = "ami-074cce78125f09d61"
   instance_type = "t3.micro"
+  subnet_id = "subnet-0134c478d258d261f"
   user_data     = file("init-script.sh")
 
   tags = {
